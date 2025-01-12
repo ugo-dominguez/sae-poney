002d1dcc7b04238efd53a71a34121924f7bf0409
Yasin <keskan543@gmail.com>
2025-01-12 19:22:25 +0100

    V0.1 Adhérent

--- login/views.py
+++ login/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+@login_required
+def adhesion(request):
+    if request.method == "POST":
+        if 'oui' in request.POST:  
+            request.user.adherent = True
+            request.user.save()
+            return redirect("home")  
+        elif 'non' in request.POST:  
+            return redirect("home")  
+    return render(request, "adhesion.html")
